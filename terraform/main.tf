218cd203f569ced3eec5ae374170da88d66e8e8d
Aaron <2559942+aaronsaderholm@users.noreply.github.com>
2021-06-25 17:49:51 -0400

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.7.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://pve1.magevent.net:8006/api2/json",
+  pm_tls_insecure = true
+  pm_log_enable = true
+  pm_log_file = "terraform-plugin-proxmox.log"
+  pm_log_levels = {
+    _default = "debug"
+    _capturelog = ""
+  }
+}
