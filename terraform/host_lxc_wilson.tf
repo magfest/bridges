d765a7fb5ea28013a8abfc91fbf7058f718d6f95
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-06-26 00:01:31 -0400

    concurrent wilson

--- terraform/host_lxc_wilson.tf
+++ terraform/host_lxc_wilson.tf
@@ -1,6 +1,7 @@
 resource "proxmox_lxc" "basic" {
+  count = 7
   target_node  = "pve1"
-  hostname     = "wilson"
+  hostname     = "wilson-${count.index}.magevent.net"
   ostemplate   = "wowza:vztmpl/ubuntu-18.04-standard_18.04.1-1_amd64.tar.gz"
   password     = "sorry"
   unprivileged = true
