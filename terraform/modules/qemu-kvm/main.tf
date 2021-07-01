2c5e7c2c9080ca1a7a0efbf5e40a33d13340587e
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-06-30 23:12:21 -0400

    feat: qemu-kvm module

---
+++ terraform/modules/qemu-kvm/main.tf
@@ -0,0 +1,18 @@
+resource "proxmox_vm_qemu" "qemu-kvm-vm" {
+    name = "VM-name"
+    target_node = "Node to create the VM on"
+    iso = "synology:iso/ubuntu-20.04.2-live-server-amd64.iso"
+    os_type = "ubuntu"
+}
+
+variable "name" {
+  description = "Name of the vm"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+  default     = "pve1"
+}
+
---
+++ terraform/qemu-livecd.tf
@@ -0,0 +1,4 @@
+module "livecd" {
+  source = "./modules/qemu-kvm"
+  name = "live-cd-test.dev.magevent.net"
+}
