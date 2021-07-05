b91cc243228ab83b73b4b81f38fc3b136375cfca
Adam Dorsey <adam.dorsey@gmail.com>
2021-07-05 17:23:34 -0400

    Revert to original module to test
    I want to see where the modules are being installed now...

--- terraform/modules/qemu-kvm/main.tf
+++ terraform/modules/qemu-kvm/main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13.0"
   required_providers {
     proxmox = {
-      source  = "registry.magevent.net/yesrod/proxmox"
-      version = ">=2.7.2"
+      source  = "Telmate/proxmox"
+      version = ">=2.7.1"
     }
   }
 }
