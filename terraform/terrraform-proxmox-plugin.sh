7d1c5f17ee23e67bd34777780bdd1d3632c1ada2
Adam Dorsey <adam.dorsey@gmail.com>
2021-07-05 13:03:28 -0400

    Add script to manually install Proxmox plugin
    - Add Bash script to download and install forked version of Terraform
      Proxmox plugin
    - Add script to before_script stage of GitLab CI config
    - Update minimum Proxmox plugin version to ensure we get the newer
      forked plugin

---
+++ terraform/terrraform-proxmox-plugin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Grab and install my version of the Terraform plugin until the HA State fix 
+# for containers is merged upstream and released.
+#
+# We should be able to adapt this to handle the intermediate period between 
+# merge and release from the upstream repository as well.
+#
+# - yesrod
+#
+PLUGIN_ARCH=linux_amd64
+
+# Build the plugin
+export GO111MODULE=on
+go install github.com/yesrod/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+cd terraform-provider-proxmox
+make build
+
+# Create the directory holding the newly built Terraform plugins
+mkdir -p ~/.terraform.d/plugins/registry.magevent.net/telmate/proxmox/2.7.2/${PLUGIN_ARCH}
+cp bin/terraform-provider-proxmox ~/.terraform.d/plugins/registry.magevent.net/telmate/proxmox/2.7.2/${PLUGIN_ARCH}/
