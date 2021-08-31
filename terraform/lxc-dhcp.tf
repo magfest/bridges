c540b083de03751c1b762706c16ac0ab628b3b71
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-08-31 02:54:03 -0400

    This one weird trick, everyone will hate it.

--- terraform/lxc-dhcp.tf
+++ terraform/lxc-dhcp.tf
@@ -2,7 +2,7 @@ module "dhcp" {
   source       = "./modules/lxc"
   count        = 2
   # This one weird trick. Everyone will hate it.
-  cluster_name = "pve${count.index % 1 + 1}"
+  cluster_name = "pve${count.index % 2 + 1}"
   ip_address   = "${cidrhost(var.subnet, 253+count.index)}"
   cidr_mask    = "${local.cidr_suffix}"
   hostname     = "dhcp${floor(count.index + 1)}.${local.domain}"
