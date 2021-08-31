6154b27c6737c4c53ccff242c287c58e53e4a1f0
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-08-31 01:48:53 -0400

    IP off by 1

--- terraform/lxc-dhcp.tf
+++ terraform/lxc-dhcp.tf
@@ -2,6 +2,6 @@ module "dhcp" {
   source       = "./modules/lxc"
   count = 2
   cluster_name = "pve1"
-  ip_address   = "${cidrhost(var.subnet, 252+count.index)}/${local.cidr_suffix}"
+  ip_address   = "${cidrhost(var.subnet, 253+count.index)}/${local.cidr_suffix}"
   hostname     = "dhcp${floor(count.index + 1)}.${local.domain}"
 }
