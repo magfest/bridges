92309f11963063532f1f9e31abd93fbb66b6125e
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-08-31 03:12:17 -0400

    Fix lxc-tftp

--- terraform/lxc-tftp.tf
+++ terraform/lxc-tftp.tf
@@ -2,7 +2,7 @@ module "tftp" {
   source       = "./modules/lxc"
   # This one weird trick. Everyone will hate it.
   cluster_name = "pve2"
-  ip_address   = "${cidrhost(var.subnet, 9}"
+  ip_address   = "${cidrhost(var.subnet, 9)}"
   cidr_mask    = "${local.cidr_suffix}"
   hostname     = "tftp.${local.domain}"
 }
