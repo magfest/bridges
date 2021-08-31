a240d9c6d0a174e420e9e1c154378abb9b61e855
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2021-08-31 03:37:15 -0400

    TOPS-102 Removed spurious comment.

--- terraform/lxc-tftp.tf
+++ terraform/lxc-tftp.tf
@@ -1,6 +1,5 @@
 module "tftp" {
   source = "./modules/lxc"
-  # This one weird trick. Everyone will hate it.
   cluster_name = "pve2"
   ip_address   = cidrhost(var.subnet, 9)
   cidr_mask    = local.cidr_suffix
