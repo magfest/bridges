4b54cc35e43101aef686c169d4575e28dd8ea3e9
Adam Dorsey <adam.dorsey@gmail.com>
2021-09-26 19:03:20 -0400

    Fix rsyslog container IP

--- terraform/lxc-rsyslog.tf
+++ terraform/lxc-rsyslog.tf
@@ -1,7 +1,7 @@
 module "rsyslog" {
   source       = "./modules/lxc"
   cluster_name = "pve2"
-  ip_address   = cidrhost(var.subnet, 13)
+  ip_address   = cidrhost(var.subnet, 130)
   gateway      = cidrhost(var.subnet, 1)
   cidr_mask    = local.cidr_suffix
   hostname     = "rsyslog.${local.domain}"
