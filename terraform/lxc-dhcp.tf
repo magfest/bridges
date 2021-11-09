c2eca98196f2da1911241f0128025d1e1bced2e5
Adam Dorsey <adam.dorsey@gmail.com>
2021-11-09 11:27:55 -0500

    fix: only one DHCP server, so I can rebase on main

--- terraform/lxc-dhcp.tf
+++ terraform/lxc-dhcp.tf
@@ -1,14 +1,13 @@
 module "dhcp" {
   source = "./modules/lxc"
-  # This one weird trick. Everyone will hate it.
   cluster_name = "pve1"
   hostname     = "dhcp1.${local.domain}"
   gateway      = cidrhost(var.subnet, 1)
   nets         = [
     {
-      ip       = cidrhost(var.subnet, 253 + count.index)
-      cidr     = local.cidr_suffix
-      tag      = "22"
+      ip   = cidrhost(var.subnet, 253)
+      cidr = local.cidr_suffix
+      tag  = "22"
     }
   ]
 }
