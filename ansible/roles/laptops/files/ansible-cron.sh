782e792b8bec1247b627cfc184834dfd290c1e00
Adam Dorsey <adam.dorsey@gmail.com>
2021-11-21 22:49:06 -0500

    Feat: add ansible-pull script

--- ansible/roles/laptops/files/ansible-cron.sh
+++ ansible/roles/laptops/files/ansible-cron.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 #
-# right now this does nothing.  eventually it'll have ansible-pull.
+# Hopefully this works.
 #
+url='https://github.com/magfest/bridges.git'
+checkout='prod'
+directory='/opt/ansible'
+logfile='/var/log/ansible-pull-update.log'
+
+mkdir ${directory}
+
+sudo ansible-pull -o -C ${checkout} -d ${directory} -i localhost -U ${url} 2>&1 | sudo tee -a ${logfile}
