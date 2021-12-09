876be8e7e134e779144bfe9c891b124581afbf59
Adam Dorsey <adam.dorsey@gmail.com>
2021-12-09 14:32:06 -0500

    fix: theme installer

--- ansible/roles/ubuntu/files/theme_install.sh
+++ ansible/roles/ubuntu/files/theme_install.sh
@@ -10,5 +10,5 @@ USERS=$(find /home -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
 for user in $USERS; do
     cp ${USER_ICON} /home/${user}/.face.png
-    su -c "gsettings set org.gnome.desktop.background picture-uri file://${WALLPAPER}" - ${user}
+    sudo -u ${user} dbus-launch gsettings set org.gnome.desktop.background picture-uri file://${WALLPAPER}
 done
