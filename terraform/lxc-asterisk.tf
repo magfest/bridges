119c5e3935d5b7026782195fd9a50b9cb0cce740
Dylan Whichard <dylan.whichard@brivo.com>
2022-01-04 21:34:43 -0500

    create asterisk tf module

---
+++ terraform/lxc-asterisk.tf
@@ -0,0 +1,13 @@
+module "asterisk" {
+  source       = "./modules/lxc"
+  cluster_name = "pve1"
+  hostname     = "asterisk.${local.domain}"
+  gateway      = cidrhost(var.subnet, 1)
+  nets = [
+    {
+      ip   = cidrhost(var.subnet, 12)
+      cidr = local.cidr_suffix
+      tag  = "22"
+    }
+  ]
+}
