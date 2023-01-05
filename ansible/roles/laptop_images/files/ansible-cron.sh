02cb58d414599ecc117399351122aa26701d7872
Aaron <2559942+aaronsaderholm@users.noreply.github.com>
2023-01-05 04:10:13 -0500

    Update homepage (#180)
    * Changes for Reg
    * Start moving laptop provisioning out of this repository.

--- ansible/roles/laptop_images/files/ansible-cron.sh
+++ ansible/roles/laptop_images/files/ansible-cron.sh
@@ -8,24 +8,41 @@ else
     FORCE=""
 fi
 
+function change_branch_to_main {
+  checkout="main"
+  cd /opt/ansible/repo
+  git checkout main
+  git pull
+  echo "main" > ${directory}/branch
+  SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+  echo "Changing branch back to main - rerunning ${SCRIPTPATH}"
+  bash ${SCRIPTPATH}
+  exit
+}
+
 url='https://github.com/magfest/bridges.git'
 directory='/opt/ansible'
-logfile='/var/log/ansible-pull-update.log'
-checkout="$(cat ${directory}/branch)"
-
-if [ "$checkout" == "" ]; then
-    checkout="main"
-    echo "main" > ${directory}/branch
+FILE=/opt/laptops/repo/ansible/playbook.yaml
+requirements=${directory}/requirements.yaml
+if [[ -f "$FILE" ]]; then
+    url='https://github.com/magfest/laptops.git'
+    echo "$FILE exists ... comfiguring from laptop repo"
+    directory='/opt/laptops'
+    requirements=${directory}/repo/ansible/requirements.yaml
 fi
 
-requirements="https://raw.githubusercontent.com/magfest/bridges/${checkout}/ansible/requirements.yaml"
+checkout="$(cat ${directory}/branch)"
 
-mkdir -p ${directory}
-wget -N -P ${directory} ${requirements}
+if [ "$checkout" == "prod" ]; then
+  change_branch_to_main
+fi
+
+if [ "$checkout" == "" ]; then
+    change_branch_to_main
+fi
 
 while ! ping -c1 google.com; do sleep 3; done
 
-ansible-galaxy install -r ${directory}/requirements.yaml
-ansible-galaxy collection install -r ${directory}/requirements.yaml
-
+ansible-galaxy install -r ${requirements}
+ansible-galaxy collection install -r ${requirements}
 ansible-pull ${FORCE} -C ${checkout} -d ${directory}/repo -i localhost, -U ${url} --tags "laptops" --vault-password-file /opt/ansible/vault-password ansible/playbook.yaml 2>&1 | tee -a ${logfile}
